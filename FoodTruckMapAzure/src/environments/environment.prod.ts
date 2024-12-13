683911526e275c2094cd13237ab9a5c65653defb
Gonzalo-Turusi <gonzalo.turusi@gmail.com>
2024-12-13 08:14:31 -0500

    Update environment.prod.ts

--- FoodTruckMapAzure/src/environments/environment.prod.ts
+++ FoodTruckMapAzure/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    azureMapsKey: '',
-    apiUrlToken: '',
-    apiUrlFoodtrucks: ''
+    azureMapsKey: 'FTCUL6EvSYMxPzglYgL8Y0tPenoOVyXUob1mIGSfSuAzP9IEgZnrJQQJ99ALACYeBjFqnTLfAAAgAZMPzgvD',
+    apiUrlToken: 'https://foodtruckfunctions.azurewebsites.net/token?',
+    apiUrlFoodtrucks: 'https://foodtruckfunctions.azurewebsites.net/foodtrucks?code=JimsvjKVJ1hJp-xAoQ_Az6sLNkwV1x165DBjuHcbdsBrAzFuW0RJnQ%3D%3D'
   };
   
